c15e880450f622edafba5e2c2e26b2f6fecc2d40
Stefany Carrillo <scarrillo7@miners.utep.edu>
2018-10-12 21:41:46 -0600

    Add files via upload

--- sampleWindow.py
+++ sampleWindow.py
@@ -1,8 +1,24 @@
-import pgi
-pgi.require_version("Gtk", "3.0")
-from pgi.repository import Gtk
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
 
-window = Gtk.Window()
+
+class MainWindow(Gtk.Window):
+
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Button Clicker 2.0")
+
+        # Button
+        self.button = Gtk.Button(label="Click here!")
+        self.button.connect("clicked", self.button_clicked)
+        self.add(self.button)
+
+    # User clicks button
+    def button_clicked(self, widget):
+        print("Gametime")
+
+
+window = MainWindow()
 window.connect("delete-event", Gtk.main_quit)
 window.show_all()
 Gtk.main()
