868e3418dedf834d2484c85c66cbfc2efa2f3670
alelicon <alejandralicon6@gmail.com>
2018-10-16 18:35:47 -0600

    fields and labels for new/modify tab of Message Type Area

--- MessageTypeArea.py
+++ MessageTypeArea.py
@@ -31,9 +31,11 @@ def Tabs():
     notebook = Gtk.Notebook()
 
     #New/Modify Page
-    newPage = Gtk.Box()
+    newPage = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
     newPage.set_border_width(10)
-    newPage.add(Gtk.Label("content Main"))
+    newPage.add(newModify())
+    #newPage.pack_start(newModify(), True, True, 0)
+    #newPage.add(Gtk.Label("content Main"))
     notebook.append_page(newPage, Gtk.Label("New/Modify"))
 
     #Dependency Page
@@ -62,3 +64,57 @@ def Tabs():
     
     tabsList.add(notebook)
     return tabs
+
+def newModify():
+
+    mainBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+    instructions = Gtk.Label("To create a new message type, please enter a message type name \n"
+                             "and select message type field value pair(s). To update/delete message type. \n"
+                             "Please select from the existing message type first and the previously \n"
+                             "selected name and field value pair(s) will be pre-populated")
+    #mainBox.add(instructions)
+
+
+    # labels
+    typeLabel = Gtk.Label("Existing Message Type")
+    nameLabel = Gtk.Label("Message Type Name")
+    fieldVPLabel = Gtk.Label("Message Type Field\n"
+                             "Value Pair(s)")
+
+    # drop down menu
+    # TODO HAVE TO ESTABLISH THE INSIDE OF THE DROPDOWN
+    typeCombo = Gtk.ComboBox()
+
+    # text boxes
+    nameEntry = Gtk.Entry()
+    fieldVPEntry = Gtk.Entry()
+
+    # bottom buttons
+    buttonBox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+    #buttonBox.set_homogeneous(True)
+    buttonBox.set_spacing(5)
+    saveBtn = Gtk.Button(label="Save")
+    deleteBtn = Gtk.Button(label="Delete")
+    clearBtn = Gtk.Button(label="Clear")
+    buttonBox.pack_start(saveBtn, True, True, 0)
+    buttonBox.pack_start(deleteBtn, True, True, 0)
+    buttonBox.pack_start(clearBtn, True, True, 0)
+
+    #grid
+    messageTypeGrid = Gtk.Grid()
+    messageTypeGrid.set_column_spacing(10)
+    messageTypeGrid.set_row_spacing(5)
+
+    messageTypeGrid.add(typeLabel)
+    messageTypeGrid.attach_next_to(typeCombo, typeLabel, Gtk.PositionType.RIGHT, 1, 1)
+    messageTypeGrid.attach_next_to(nameLabel, typeLabel, Gtk.PositionType.BOTTOM, 1, 1)
+    messageTypeGrid.attach_next_to(nameEntry, nameLabel, Gtk.PositionType.RIGHT, 1, 1)
+    messageTypeGrid.attach_next_to(fieldVPLabel, nameLabel, Gtk.PositionType.BOTTOM, 1, 1)
+    messageTypeGrid.attach_next_to(fieldVPEntry, fieldVPLabel, Gtk.PositionType.RIGHT, 1, 1)
+    messageTypeGrid.attach_next_to(buttonBox, fieldVPEntry, Gtk.PositionType.BOTTOM, 2, 1)
+
+    mainBox.pack_start(instructions, True, True, 0)
+    mainBox.pack_start(messageTypeGrid,True,True,0)
+    #mainBox.pack_start(buttonBox, True, True, 0)
+
+    return mainBox
