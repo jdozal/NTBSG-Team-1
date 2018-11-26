1ccae07eb2aa6427383c9f3253030940daef9fdc
System Administrator <root@usgs-admins-MacBook-Air.local>
2018-11-25 22:57:55 -0700

    Command working, estoy bien mensa estaba mas facil de lo que pensaba perdonenme xoxo

--- PCAP.py
+++ PCAP.py
@@ -6,14 +6,15 @@ Created on Wed Nov 21 17:05:39 2018
 @author: Jessica Dozal
 """
 
-import pyshark
+import os 
 
 class PCAP:
     name = ''
     filePath= ' '
     
     def convert():
-        cap = pyshark.FileCapture('/Users/jdozal/Documents/Fall2018/SOFTWARE/PCAP/maccdc2012_00000.pcap')
-        print(cap[0])
-        
+        # PCAP to PDML 
+        cmd = "tshark -r test.pcap > testPDML.pdml -T pdml"
+        os.system(cmd)
+    
     convert()
