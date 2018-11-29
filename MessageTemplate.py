78ad9e45d701e1266a7d8f306bad8871d8e37a77
scarrillo9 <scarrillo7@miners.utep.edu>
2018-11-28 19:07:09 -0700

    messagetemplate skeleton

--- MessageTemplate.py
+++ MessageTemplate.py
@@ -1,6 +1,35 @@
 
 class MessageTemplate:
-    name=''
-    destinationName=''
-    destinationPath=''
-    outputFormat=''
+    def __init__(self, name, destinationName, destinationPath, outputFormat):
+        self.name = name
+        self.destinationName = destinationName
+        self.destinationPath = destinationPath
+        self.outputFormat = outputFormat
+
+    @property
+    def name(self):
+        return self.__name
+    @name.setter
+    def sourceMessageType(self, name):
+        self.__name = name
+
+    @property
+    def destinationName(self):
+        return self.__destinationName
+    @destinationName.setter
+    def destinationName(self, destinationName):
+        self.__destinationName = destinationName
+
+    @property
+    def destinationPath(self):
+        return self.__destinationPath
+    @destinationPath.setter
+    def targetMessageType(self, destinationPath):
+        self.__destinationPath = destinationPath
+
+    @property
+    def outputFormat(self):
+        return self.__outputFormat
+    @outputFormat.setter
+    def outputFormat(self, outputFormat):
+        self.__outputFormat = outputFormat
