4e49c30e7bcad2faefcd92ec2acebf52b87aa66d
alelicon <alejandralicon6@gmail.com>
2018-12-09 22:17:47 -0700

    Added Filter and filter's container classes

---
+++ Filter.py
@@ -0,0 +1,16 @@
+"""
+Created on Sun Dec 9
+
+@author: Alejandra Licon
+"""
+import xml.etree.ElementTree as ET
+import FiltersContainer
+
+class Filter:
+
+    def __init__(self, name, expression):
+        self.name = name
+        self.expression = expression
+
+
+
